bd05d1ed2881ffec84e4afef28d656b15cd7594e
Julien Fougery <web@fougery.ovh>
2021-03-12 09:56:42 +0100

    Create Level.lua

---
+++ objects/Level.lua
@@ -0,0 +1,20 @@
+
+
+local Level = {}
+
+
+-- =================================================================== --
+-- DEF Level
+
+-- sprite background
+-- son d'ambiance
+
+
+-- Pattern d'ennemies
+-- un boss
+
+
+
+
+
+return Level
